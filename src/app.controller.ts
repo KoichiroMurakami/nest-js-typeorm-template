bbbecdba7ca3bddda45681cb1a0abeef4db55c7a
koichiro murakami <koichiromurakami@koichironoMacBook-Air.local>
2021-09-19 18:59:54 +0900

    created form sample app

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,25 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Body, Controller, Get, Post, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/')
   @Render('index')
   root() {
     return {
-      title: 'Nest sample',
-      header: 'Nest js header',
-      message: 'hello'
-    };
+      title: 'Nest sample app',
+      msg: 'hello'
+    }
+  }
+
+  @Post('/')
+  @Render('index')
+  send(@Body() form:any) {
+    return {
+      title: 'NestForm',
+      msg: JSON.stringify(form)
+    }
   }
 }
